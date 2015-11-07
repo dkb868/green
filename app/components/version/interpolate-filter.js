8eb713a534d94da410ccfa4dbb130161eda6c0f9
Brian Tan <tanrui.brian@gmail.com>
2015-11-07 12:15:55 -0800

    Deleted shit that didn't work

--- app/components/version/interpolate-filter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('green.version.interpolate-filter', [])
-
-.filter('interpolate', ['version', function(version) {
-  return function(text) {
-    return String(text).replace(/\%VERSION\%/mg, version);
-  };
-}]);
--- app/components/version/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('green.version', [
-  'green.version.interpolate-filter',
-  'green.version.version-directive'
-])
-
-.value('version', '0.1');
